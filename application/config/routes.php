3824f39202bb3a93f46527e088cf316f935a3150
cajoue <cajoue@users.noreply.github.com>
2018-02-10 16:28:59 +0100

    create routing to use short link address: /about instead of: /pages/view/about

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+// change default controller from welcome to Pages
+$route['default_controller'] = 'pages/view';
+// go to any page we want, if don't enter a page will go to home by default
+$route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
