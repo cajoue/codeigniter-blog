97b64b3b97d19d84ff1b0cc3b398b99ed2a22298
cajoue <cajoue@users.noreply.github.com>
2018-02-10 18:57:57 +0100

    feat: Add get_posts function

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -4,6 +4,17 @@
 		public function __construct() {
 			$this->load->database();
 		}
-
-}
+		
+		public function get_posts($slug = FALSE) {
+			// if slug not passed
+			if ($slug === FALSE) {
+				// using Active Model in codeigniter
+				$query = $this->db->get('posts');
+				return $query->result_array();
+			}
+			// if a slug is passed
+			$query = $this->db->get_where('posts', array('slug'=>$slug));
+			return $query->row_array();
+		}
+	}
 	
