33de4fe2e505b81f0923845f07fbe35dae00f415
cajoue <cajoue@users.noreply.github.com>
2018-02-10 17:27:39 +0100

    Enable autoload url helper

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codeIgniter-blog';
+$config['base_url'] = 'http://localhost:8888/codeIgniter-blog';
 
 /*
 |--------------------------------------------------------------------------
