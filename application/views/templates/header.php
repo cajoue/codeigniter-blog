cd92688216103b098709100919822dbeb1f0950f
cajoue <cajoue@users.noreply.github.com>
2018-02-10 17:34:18 +0100

    Style: Add bootswatch theme and simple navigation

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,19 @@
 <html>
-    <head>
-        <title>CodeIgniter Blog</title>
-    </head>
-    <body>
+  <head>
+    <title>CodeIgniter Blog</title>
+    <link rel='stylesheet' href='https://bootswatch.com/4/materia/bootstrap.min.css'>
+  </head>
+  <body>
+  <nav class="navbar navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="<?php echo base_url(); ?>">CodeIgniter Blog</a>
+      </div>
+      <div id="navbar">
+        <ul class="nav navbar-nav">
+          <li><a href="<?php echo base_url(); ?>">Home</a></li>
+          <li><a href="<?php echo base_url(); ?>about">About</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
