f637f63546df678d6e0d7cd2604c9da2a8512226
cajoue <cajoue@users.noreply.github.com>
2018-02-11 21:14:39 +0100

    style: fix navbar styles

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,22 +1,28 @@
 <html>
   <head>
     <title>CodeIgniter Blog</title>
-    <link rel='stylesheet' href='https://bootswatch.com/4/materia/bootstrap.min.css'>
+		<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0-beta.3/flatly/bootstrap.min.css'>
+		<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">-->
 		<link rel='stylesheet' href='<?php echo base_url(); ?>/assets/css/style.css'>
   </head>
   <body>
-  <nav class="navbar navbar-inverse">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
       <div class="navbar-header">
         <a class="navbar-brand" href="<?php echo base_url(); ?>">CodeIgniter Blog</a>
       </div>
-      <div id="navbar">
-        <ul class="nav navbar-nav">
-          <li><a href="<?php echo base_url(); ?>">Home</a></li>
-          <li><a href="<?php echo base_url(); ?>about">About</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+          <a class="nav-item nav-link" href="<?php echo base_url(); ?>">Home</a>
+          <a class="nav-item nav-link" href="<?php echo base_url(); ?>about">About</a>
+					<a class="nav-item nav-link" href="<?php echo base_url(); ?>posts">Blog</a>      
+				</div>
+			</div>
+		</div>
+  </nav>		
+				
   <div class='container'>
 			
