5e19c11309b4cc81fe238533598d64336cba1a25
cajoue <cajoue@users.noreply.github.com>
2018-02-24 20:25:04 +0100

    feat: add bootstrap form to create a post

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,2 +1,12 @@
 <h2><?= $title; ?></h2>
-
+<form>
+  <div class="form-group">
+    <label>Title</label>
+    <input type="text" class="form-control" name="title" placeholder="Add Title">
+  </div>
+  <div class="form-group">
+    <label>Content</label>
+    <textarea class="form-control" name="body" placeholder="Your content here"></textarea>	
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
