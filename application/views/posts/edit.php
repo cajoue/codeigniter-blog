e42c76d014f2dc70a3fff8cf1e0206f87f67417c
cajoue <cajoue@users.noreply.github.com>
2018-02-25 16:15:18 +0100

    feat: Show current post data in edit form

--- application/views/posts/edit.php
+++ application/views/posts/edit.php
@@ -1,13 +1,13 @@
 <h2><?= $title; ?></h2>
 <?php echo validation_errors(); ?>
-<?php echo form_open('posts/edit'); ?>
+<?php echo form_open('posts/update'); ?>
   <div class="form-group">
     <label>Title</label>
-    <input type="text" class="form-control" name="title" placeholder="Add Title">
+    <input type="text" class="form-control" name="title" placeholder="Add Title" value="<?php echo $post['title']; ?>">
   </div>
   <div class="form-group">
     <label>Content</label>
-    <textarea class="form-control" name="body" placeholder="Your content here"></textarea>	
+    <textarea class="form-control" name="body" placeholder="Your content here"><?php echo $post['body']; ?></textarea>	
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 <?php	echo form_close(); ?>
