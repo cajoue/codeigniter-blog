3886fecc16171c7496311b5783ca46df85f9ce23
cajoue <cajoue@users.noreply.github.com>
2018-02-25 13:22:04 +0100

    feat: Add edit button to view

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -4,6 +4,7 @@
 	<?php echo $post['body']; ?>
 </div>
 <hr>
+<a href="/posts/edit/<?php echo $post['slug']; ?>" class="btn btn-warning float-left">Edit</a>
 <?php echo form_open('/posts/delete/' . $post['id']); ?>
 	<input type="submit" value="Delete" class="btn btn-danger">	
 <?php echo form_close(); ?>
